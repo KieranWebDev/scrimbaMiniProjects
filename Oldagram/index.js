ea3021635842b940688812fa3d181bde4135bc31
Kieran <kieran.webdevelopment@gmail.com>
2023-03-23 16:37:06 +0000

    like button functional

--- Oldagram/index.js
+++ Oldagram/index.js
@@ -1,63 +1,60 @@
 import posts from './data.js';
 
-const name = document.querySelector('#name');
-const username = document.querySelector('#username');
-const location = document.querySelector('#location');
-const avatar = document.querySelector('#avatar');
-const post = document.querySelector('#post');
-const comment = document.querySelector('#comment');
-const likes = document.querySelector('#likes');
+// const name = document.querySelector('#name');
+// const username = document.querySelector('#username');
+// const location = document.querySelector('#location');
+// const avatar = document.querySelector('#avatar');
+// const post = document.querySelector('#post');
+// const comment = document.querySelector('#comment');
+
 // const container = document.querySelector('.container');
 let body = document.querySelector('body');
 
-const testing = posts[0].name;
-console.log(testing);
-
 posts.forEach((post) => {
-  const likebtn = document.createElement('button');
-  likebtn.setAttribute('id', 'like-btn');
-  likebtn.addEventListener('click', () => {
-    post.likes += 1;
-    console.log(likes);
-  });
-  console.log(likebtn);
-  renderList(post);
-});
-
-function renderList(post) {
   const container = document.createElement('div');
-  container.classList.add('container');
 
+  container.classList.add('container');
   container.innerHTML = `
-<header>
-    <img class="logo" src="images/logo.png">
-    <img class="avatar-img" src="./images/avatar-courbet.jpg" alt=""/>
-</header>
-<section>
-    <div class="userName-container">
-        <img class="avatar-img" src="./images/avatar-vangogh.jpg" alt="van gogh picture" />
-        <div class="userName">
-            <h2 id="name">${post.name}</h2>
-            <p id="location">${post.location}</p>
-        </div>
-    </div>
-    <img id="post" class="post-pic" src="${post.post}" alt="" class="main-img" />
-    <div class="info-and-button-container">
-        <div class="icons">
-            <button id="like-btn"><img src="./images/icon-heart.png" alt=""/></button>
-            <img src="./images/icon-comment.png" alt="" />
-            <img src="./images/icon-dm.png" alt="" />
-        </div>
-        <p id="likes" class="bold">${post.likes} likes</p>
-        <p><span id='username' class="bold">${post.username} </span>${post.comment}</p>
-    </div>
+  <header>
+  <img class="logo" src="images/logo.png">
+      <img class="avatar-img" src="./images/avatar-courbet.jpg" alt=""/>
+      </header>
+  <section>
+      <div class="userName-container">
+          <img class="avatar-img" src="./images/avatar-vangogh.jpg" alt="van gogh picture" />
+          <div class="userName">
+          <h2 id="name">${post.name}</h2>
+          <p id="location">${post.location}</p>
+          </div>
+      </div>
+      <img id="post" class="post-pic" src="${post.post}" alt="" class="main-img" />
+      <div class="info-and-button-container">
+          <div class="icons">
+              <button id="like-btn"><img src="./images/icon-heart.png" alt=""/></button>
+              <img src="./images/icon-comment.png" alt="" />
+              <img src="./images/icon-dm.png" alt="" />
+          </div>
+          <p id="likes" class="bold">${post.likes} likes</p>
+          <p><span id='username' class="bold">${post.username} </span>${post.comment}</p>
+      </div>
 </div>
 </section>
 `;
   body.appendChild(container);
-}
-// console.log(container);
+});
 
+const likebtn = document.querySelectorAll('#like-btn');
+const likes = document.querySelectorAll('#likes');
+
+likebtn.forEach((btn, index) => {
+  btn.addEventListener('click', () => {
+    let newLikeCount = (posts[index].likes += 1);
+    likes[index].textContent = `${newLikeCount} likes`;
+    console.log(posts[index].likes);
+  });
+});
+
+// console.log(container);
 // const posts = [
 //     {
 //         name: "Vincent van Gogh",
